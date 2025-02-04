678c28343e18cbc7a7bda556cafa95da24339552
JuanVeranoMesa <juanho2007@gmail.com>
2025-02-03 21:41:28 -0500

    Another day's work; added a few screen size functions and imported them into main

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
+import pygame # type: ignore
+
+from constants import *
 
 def main():
     print("Starting asteroids!")
+    
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
