dc4876d12698c57a5405a8313b051164edbd7e2c
JuanVeranoMesa <juanho2007@gmail.com>
2025-02-05 20:40:26 -0500

    Working on adding collision

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,11 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+    
+    def collision(self, target):
+        #establish the vector positions for distance
+        v1 = pygame.vector2(self.position)
+        v2 = pygame.vector2(target.position)
+        
+        #find distance, return True if distance is 
+        if self.radius
