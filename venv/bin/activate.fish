35cff719c9b012291a9bf7340a9693bc8cd05275
JuanVeranoMesa <juanho2007@gmail.com>
2025-02-06 21:44:48 -0500

    This is fine

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/juanverano/workspace/github.com/JuanVeranoMesa/asteroid/venv"
+set -gx VIRTUAL_ENV "/Users/juanverano/workspace/github/JuanVeranoMesa/asteroid/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
